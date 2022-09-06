681416d86be21052e2a8e0645d4048162dbd7298
GilmarPeron <109175794+DevJuniorP@users.noreply.github.com>
2022-09-06 14:39:24 -0400

    commit semanal
    06/09/2022

---
+++ projetodesite/js/script.js
@@ -0,0 +1,13 @@
+var el = document.querySelector('.menu-mobile-icon')
+
+el.addEventListener('click', () => {
+    //Queremos que apareça o menu.
+    var menuItems = document.querySelector('.menu-items')
+    if(menuItems.classList.contains('show')){
+        menuItems.classList.add('hide')
+        menuItems.classList.remove('show')
+    }else{
+        menuItems.classList.add("show")
+        menuItems.classList.remove('hide')
+    }
+})
