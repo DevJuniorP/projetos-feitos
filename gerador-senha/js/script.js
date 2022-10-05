91bdfe3fb357f75ac904dd62d32521ca55800194
GilmarPeron <109175794+DevJuniorP@users.noreply.github.com>
2022-10-05 13:44:07 -0400

    grud
    05/10/2022

--- gerador-senha/js/script.js
+++ gerador-senha/js/script.js
@@ -1,58 +1,60 @@
-const passInput = document.querySelector("#inputPasswordId");
-const lenInput = document.querySelector("#inputLengthId");
-const infoLength = document.querySelector('label[for="inputLengthId"]');
+//VARIÁVEIS EM GERAL
+
+const passInput = document.querySelector("#inputPassword");
+const lenInput = document.querySelector("#inputLength");
+const labLength = document.querySelector('label[for="inputLength"]');
 const btnGerar = document.querySelector("#btnGerar");
 
-const chkLower = document.querySelector("#chkLowerId");
-const chkUpper = document.querySelector("#chkUpperId");
-const chkNumber = document.querySelector("#chkNumberId");
-const chkSymbols = document.querySelector("#chkSymbolsId");
+//CHECKBOX
 
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const symbols = ["!", "@", "#", "$", "%"];
+const checkLower = document.querySelector("#checkLower");
+const checkUpper = document.querySelector("#checkUpper");
+const checkNumbers = document.querySelector("#checkNumbers");
+const checkSymbols = document.querySelector("#checkSymbols");
+const aviso = document.querySelector("#aviso");
+
+//CARACTERS
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const symbols = ["@", "!", "#", "$", "%", "&"]
 
 const caracters = Array.from(Array(26)).map((_, i) => i + 97);
 const LowercaseCaracters = caracters.map((item) => String.fromCharCode(item));
 const UppercaseCaracters = LowercaseCaracters.map((item) => item.toUpperCase());
 
-infoLength.innerHTML = lenInput.value;
+labLength.innerHTML = lenInput.value;
 
 lenInput.addEventListener("change", () => {
-  infoLength.innerHTML = lenInput.value;
-});
+   labLength.innerHTML = lenInput.value;
+})
 
 btnGerar.addEventListener("click", () => {
-  generatePassword(
-    chkNumber.checked,
-    chkSymbols.checked,
-    chkLower.checked,
-    chkUpper.checked,
-    lenInput.value
-  );
+   generatePassword(
+      checkLower.checked,
+      checkUpper.checked,
+      checkNumbers.checked,
+      checkSymbols.checked,
+      lenInput.value
+   );
 });
 
-const generatePassword = (
-  hasNumbers,
-  hasSymbols,
-  hasLowercase,
-  hasUppercase,
-  lenght
-) => {
-  const newArray = [
-    ...(hasNumbers ? numbers : []),
-    ...(hasSymbols ? symbols : []),
-    ...(hasLowercase ? LowercaseCaracters : []),
-    ...(hasUppercase ? UppercaseCaracters : []),
-  ];
+function generatePassword (hasLowercase, hasUppercase, hasNumbers, hasSymbols, length) {
+   const newArray = [
+      ...(hasLowercase ? LowercaseCaracters : []),
+      ...(hasUppercase ? UppercaseCaracters : []),
+      ...(hasNumbers ? numbers : []),
+      ...(hasSymbols ? symbols : [])
+   ];
 
-  if (newArray.length === 0) return;
-
-  let password = "";
-
-  for (let i = 0; i < lenght; i++) {
-    const randomIndex = Math.floor(Math.random() * newArray.length);
-    password += newArray[randomIndex];
-  }
-
-  passInput.value = password;
-};
+   if(newArray.length === 6 ){
+      return console.log('error')
+   }else{
+      let password = "";
+      for(let i = 0; i < length; i++){
+         const randomIndex = Math.floor(Math.random() * newArray.length);
+         password += newArray[randomIndex];
+      }
+      passInput.value = password;  
+   }
+    
+}
